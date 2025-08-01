7ec31ad8e9390a27897a178adabdffa57f334a2d
Hecheng Yu <kde-yyds@qq.com>
2025-08-01 09:37:16 +0800

    Revert "InputConsumer: Allow null consume time"
    This reverts commit 546f59877543c9d6a0d6e5ad8352263d57185061.

--- libs/input/InputConsumer.cpp
+++ libs/input/InputConsumer.cpp
@@ -810,13 +810,12 @@ status_t InputConsumer::sendTimeline(int32_t inputEventId,
 
 nsecs_t InputConsumer::getConsumeTime(uint32_t seq) const {
     auto it = mConsumeTimes.find(seq);
-    if (it == mConsumeTimes.end()) {
-        ALOGE("Could not find consume time for seq=%" PRIu32, seq);
-        return 0;
-    }
+    // Consume time will be missing if either 'finishInputEvent' is called twice, or if it was
+    // called for the wrong (synthetic?) input event. Either way, it is a bug that should be fixed.
+    LOG_ALWAYS_FATAL_IF(it == mConsumeTimes.end(), "Could not find consume time for seq=%" PRIu32,
+                        seq);
     return it->second;
 }
-
 
 void InputConsumer::popConsumeTime(uint32_t seq) {
     mConsumeTimes.erase(seq);
--- libs/input/InputConsumerNoResampling.cpp
+++ libs/input/InputConsumerNoResampling.cpp
@@ -31,7 +31,6 @@
 #include <input/InputConsumerNoResampling.h>
 #include <input/PrintTools.h>
 #include <input/TraceTools.h>
-#include <log/log.h>
 
 namespace android {
 
@@ -324,10 +323,10 @@ void InputConsumerNoResampling::reportTimeline(int32_t inputEventId, nsecs_t gpu
 
 nsecs_t InputConsumerNoResampling::popConsumeTime(uint32_t seq) {
     auto it = mConsumeTimes.find(seq);
-    if (it == mConsumeTimes.end()) {
-        ALOGE("Could not find consume time for seq=%" PRIu32, seq);
-        return 0;
-    }
+    // Consume time will be missing if either 'finishInputEvent' is called twice, or if it was
+    // called for the wrong (synthetic?) input event. Either way, it is a bug that should be fixed.
+    LOG_ALWAYS_FATAL_IF(it == mConsumeTimes.end(), "Could not find consume time for seq=%" PRIu32,
+                        seq);
     nsecs_t consumeTime = it->second;
     mConsumeTimes.erase(it);
     return consumeTime;
